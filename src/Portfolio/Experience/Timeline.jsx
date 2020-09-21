29e440916860c1098c221c13736c81cc4585272d
devsagarkhatri <dev.sagarkhatri@gmail.com>
2020-09-22 02:11:44 +0530

    bg-image in Welcome module and Projects module

--- src/Portfolio/Experience/Timeline.jsx
+++ src/Portfolio/Experience/Timeline.jsx
@@ -10,13 +10,13 @@ class Timeline extends Component {
             <br />
             <div className="col-sm-9">
               <div className="list">
-                <h3>{data1.company}</h3>
+                <h4>{data1.company}</h4>
                 <br />
                 <h5>
                   {data1.role},{" " + data1.tenure}
                 </h5>
                 <hr />
-                <h5 className="desc1">{'"' + data1.desc + '"'}</h5>
+                <h6 className="desc1">{'"' + data1.desc + '"'}</h6>
               </div>
             </div>
             <div className="col-sm-3">
